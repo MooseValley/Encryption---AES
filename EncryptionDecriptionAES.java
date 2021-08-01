040a98d093293406e2acf5b768ecbdc77cb78eaa
MooseValley <Moose_Software@yahoo.com.au>
2021-08-01 14:56:03 +1000

    v0.02
    v0.02

--- EncryptionDecriptionAES.java
+++ EncryptionDecriptionAES.java
@@ -12,7 +12,8 @@ import java.util.Base64;
 import java.util.Base64.Encoder;
 import javax.crypto.spec.SecretKeySpec;
 
-public class EncryptionDecrptionAES {
+public class EncryptionDecriptionAES
+{
 
     private static final String ALGO = "AES";
   //private static final String KEY_STR = "s@IlIngkePPel";     // ERROR: Must be 16 bytes.
@@ -20,32 +21,49 @@ public class EncryptionDecrptionAES {
   //private static final String KEY_STR = "TheRooKnatEnirin";
 
     //private static final byte[] keyValue = new byte[]{'T', 'h', 'e', 'R', 'o', 'o', 'K', 'n', 'a', 't','E','n', 'i', 'r','i','n'};
-    private static final byte[] keyValue = KEY_STR.getBytes();
+    private static       byte[] keyValue = KEY_STR.getBytes();
 
-    public EncryptionDecrptionAES(){
+    public EncryptionDecriptionAES(){
 
     }
 
-    public static String setEncryptedString(String data) throws Exception
+    public static boolean setKeyValue (String newKeyStr)
+    {
+       boolean result = false; // ERROR;
+
+       if ((newKeyStr != null) && (newKeyStr.length() == 16) )
+       {
+          keyValue = newKeyStr.getBytes();
+          result = true; // All OK.
+       }
+       else
+       {
+          System.out.println ("ERROR: Invalid key '" + newKeyStr + "' - must be 16 chars long.");
+       }
+
+       return result;
+    }
+
+    public static String encryptString (String dataStr) throws Exception
     {
         Key key = getKey();
         Cipher cipher = Cipher.getInstance(ALGO);
         cipher.init(Cipher.ENCRYPT_MODE, key);
-        byte[] encryptedValue = cipher.doFinal(data.getBytes("UTF-8"));
+        byte[] encryptedValue = cipher.doFinal(dataStr.getBytes("UTF-8"));
 
         //return Base64.encodeToString(encryptedValue, Base64.DEFAULT);
         return Base64.getEncoder().encodeToString(encryptedValue);
     }
 
-    public static String getDecryptedValue(String data) throws Exception
+    public static String decryptString (String dataStr) throws Exception
     {
-        if(data != null)
+        if(dataStr != null)
         {
             Key key = getKey();
             Cipher cipher = Cipher.getInstance(ALGO);
             cipher.init(Cipher.DECRYPT_MODE, key);
             //byte[] decodebyte = Base64.decode(data.getBytes("UTF-8"), Base64.DEFAULT);
-            byte[] decodebyte = Base64.getDecoder().decode(data.getBytes("UTF-8"));
+            byte[] decodebyte = Base64.getDecoder().decode(dataStr.getBytes("UTF-8"));
 
             byte[] decValue = cipher.doFinal(decodebyte);
 
@@ -58,6 +76,18 @@ public class EncryptionDecrptionAES {
     private static Key getKey() throws Exception
     {
         return new SecretKeySpec(keyValue, ALGO);
+    }
+
+    private static String getKeyString()
+    {
+       String resultStr = "";
+
+       for (int k = 0; k < keyValue.length; k++)
+       {
+          resultStr += "" + (char) keyValue [k];
+       }
+
+       return resultStr;
     }
 
     public static void main (String[] args)
@@ -74,21 +104,19 @@ public class EncryptionDecrptionAES {
 
        try
        {
-          encryptedStr = setEncryptedString (originalStr);
-          decryptedStr = getDecryptedValue  (encryptedStr);
+          encryptedStr = encryptString (originalStr);
+          decryptedStr = decryptString (encryptedStr);
        }
        catch (Exception err)
        {
           System.out.println ("Error: " + err.toString());
        }
 
+       System.out.println ("Key           = " + getKeyString() );
        System.out.println ("Key length    = " + keyValue.length + " bytes (must be 16 bytes).");
        System.out.println ("originalStr   = " + originalStr);
        System.out.println ("encryptedStr  = " + encryptedStr);
        System.out.println ("decryptedStr  = " + decryptedStr);
-
-
     }
+} // public class EncryptionDecriptionAES
 
-
-}
